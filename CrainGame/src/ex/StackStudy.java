04072be71afdd67865c9049aed1d65e8e743189b
qkrwlsaud <qkrwlsaud0118@gmail.com>
2020-05-14 00:31:36 +0900

    Stack Ex

--- CrainGame/src/ex/StackStudy.java
+++ CrainGame/src/ex/StackStudy.java
@@ -8,10 +8,12 @@ public class StackStudy {
 
 		Stack<Integer> s=new Stack<Integer>();
 		
-		s.push(1);
+		//데이터 담음
+		s.push(1); 
 		s.push(2);
 		s.push(3);
 		
+		//데이터 꺼내기 
 		int num=s.pop();
 		System.out.println(num);
 		

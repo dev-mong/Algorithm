278a76bf8dacada3d07c99e00b36a2e122ef2f1d
qkrwlsaud <qkrwlsaud0118@gmail.com>
2020-05-14 13:18:10 +0900

    delete

--- CrainGame/src/ver03/Solution3.java
+++ CrainGame/src/ver03/Solution3.java
@@ -11,51 +11,8 @@ public class Solution3 {
 		int answer=0; // 인형 개수 
 		
 		Stack<Integer> s=new Stack<Integer>();
-		Stack<Integer> s2=new Stack<Integer>();
-		Stack<Integer> s3=new Stack<Integer>();
-		Scanner sc=new Scanner(System.in);
-		
-		board[0][0]=s.push(0);
-		board[0][1]=s.push(1);
-		board[0][2]=s.push(2);
-		
-		board[1][0]=s.push(0);
-		board[1][1]=s.push(1);
-		board[1][2]=s.push(4);
-		
-		board[2][0]=s.push(2);
-		board[2][1]=s.push(5);
-		board[2][2]=s.push(3);
 		
 		
-		
-		//인형을 보드에 담음
-//		int j=0;
-		for(int i=0;i<3;i++) {
-	    	for(int j=0;j<3;j++) {
-////	    		int doll = (int)(Math.random()*101); 
-//	    			board[i][j]=s;//인형의 종류를 담음
-	    		  	System.out.print(board[i][j] +" "); 
-		    	}System.out.println("\t");
-//		    	System.out.println(s);
-	    }
-
-		
-		for(int k=0;k<3;k++) { //행을 뽑음 - 크레인 위치 이동
-		System.out.println("크레인을 이동 할 위치를 입력하세요. 1~5사이로 입력 ");
-		moves[k]=sc.nextInt();
-		sc.nextLine();
-		
-		for(int i=0;i<3;i++) {
-				if(board[moves[k]-1][i]==s.peek()) { //인형이 이동한 위치의 마지막 데이터 값 비교 
-					System.out.println(s.peek());
-					answer+=1;
-					s.pop();
-				}
-		
-			}
-				
-			}
 		
 		return answer;
 	}
@@ -70,6 +27,26 @@ public class Solution3 {
 		int[] moves=new int[5]; //1000으로 바꾸기 
 		
 		int dollCount=0;
+		
+		Scanner sc=new Scanner(System.in);
+		
+		//인형을 보드에 담음
+		for(int i=0;i<3;i++) {
+	    	for(int j=0;j<3;j++) {
+	    		int doll = (int)(Math.random()*101); 
+	    			board[i][j]=doll;//인형의 종류를 담음
+	    		  	System.out.print(board[i][j] +" "); 
+		    	}System.out.println("\t");
+	    }
+		
+		for(int k=0;k<moves.length;k++) {
+		System.out.println("이동할 위치 선택 1~5");
+			int location=sc.nextInt();
+			sc.nextLine();
+			moves[k]=location;
+		}
+	
+		
 		//함수 호출
 		dollCount = soulution(board,moves);
 		

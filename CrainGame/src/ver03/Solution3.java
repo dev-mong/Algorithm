c79413c86337c02fa7eaabd6faed6e02283b8aff
qkrwlsaud <qkrwlsaud0118@gmail.com>
2020-05-18 23:29:46 +0900

    craingame

--- CrainGame/src/ver03/Solution3.java
+++ CrainGame/src/ver03/Solution3.java
@@ -16,25 +16,27 @@ public class Solution3 {
 			
 			for( int j=0;j<board.length;j++) { //행 변화 
 				
-				if(board[j][moves[i]-1] !=00) { //이동한 위치에 인형이 있을 때 
+				if(board[j][moves[i]-1] !=0) { //이동한 위치에 인형이 있을 때 
 					
 					if(sBucket.isEmpty()) {
 						sBucket.push(board[j][moves[i]-1]); //현위치의 인형 종류 저장
-					}else {
+					}
+					
+					else {
 						if(sBucket.peek()==board[j][moves[i]-1]) { //맨 마지막 넣은 인형 종류가 현재 인형 종류와 같을 때
 							sBucket.pop(); //같은 인형 종류 이므로 삭제
-//							answer=+2;
+							answer=+2;
 						}else{
 							sBucket.push(board[j][moves[i]-1]);
-							answer=+1;
 						}
 					}
+					board[j][moves[i]-1]=0;
+					break;
+					
 				}
 
 				}
-			}
-		
-		return answer;
+			}		return answer;
 	}
 
 	public static void main(String[] args) {
@@ -54,12 +56,6 @@ public class Solution3 {
 		for(int i=0;i<board.length;i++) {
 	    	for(int j=0;j<board[i].length;j++) {
 	    		int doll = (int)(Math.random()*101); 
-	    		
-	    		if(doll == 0) {
-	    			board[i][j]=00; //인형이 없을 떄 
-	    		} else {  		
-	    			board[i][j]=doll;//인형의 종류를 담음
-	    		}
 	    		System.out.print(board[i][j] +" "); 
 		    	}System.out.println("\t");
 	    }

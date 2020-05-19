221570daeb7b10b3b97f62d7d5097d6a8673a2c3
qkrwlsaud <qkrwlsaud@gamil.com>
2020-05-19 17:36:32 +0900

    ver 0.2

--- Level1_EX1/src/sum/SolutionVer1.java
+++ Level1_EX1/src/sum/SolutionVer1.java
@@ -4,28 +4,26 @@ public class SolutionVer1 {
 
 	//두 정수 a,b 사이에 속한 모든 정수의 합 
 	public static long solution(int a,int b) {
-	
 		long answer=0;
 		
+		
 		while(true) {
-		if(a>b) {
-			answer+=a;
+			answer+=b;
+			if(a<=b) {
+			return answer;	
+			} 
 			b++;
-			
-		}
-		if(a==b) {
-			answer=a;
-			return answer;
-		}
-		return answer;
 		}
 		
 	}
 	
+	
+	
 	public static void main(String[] args) {
 
-		int a=3;
-		int b=5;
+		int a=5;
+		int b=3;
+		
 		
 		long result = solution(a,b);
 		System.out.println(result);

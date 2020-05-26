ee50c19fd87b36de627425071a0455b87698c155
qkrwlsaud <qkrwlsaud0118@gmail.com>
2020-05-26 14:44:48 +0900

    자릿수

---
+++ Level1_EX1/src/ex5_numberSum/Solution1.java
@@ -0,0 +1,22 @@
+package ex5_numberSum;
+
+public class Solution1 {
+
+	 public static int solution(int n) {
+	        int answer = 0;
+
+	        answer=123/100;
+	        
+
+	        return answer;
+	    }
+	
+	public static void main(String[] args) {
+
+		
+		int a=123;
+		a=solution(a);
+		System.out.println(a);
+	}
+
+}

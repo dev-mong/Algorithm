d2fe4a2f13c448825b9baa974d143193862bbd1e
qkrwlsaud <qkrwlsaud0118@gmail.com>
2020-06-05 12:36:16 +0900

    문자열 기초

--- Level1_EX1/src/ex7_string/Solution1.java
+++ Level1_EX1/src/ex7_string/Solution1.java
@@ -5,22 +5,25 @@ public class Solution1 {
 	
 	public static boolean solution(String s) {
 		
-		boolean answer = true;
-        
-		for(int i=0;i<s.length();i++) {
-		if(s.length()<4  s.length()>6 || 48>s.charAt(i) || s.charAt(i)>57) {
-			answer=false;
-		} 
-		}
-        
-        return answer;
+		
+		if(s.length()<4 || s.length()>6) {
+			
+			for(int i=0;i<s.length();i++) {
+				if( 48>s.charAt(i) || s.charAt(i)>57) {
 
+				System.out.println(s.charAt(i));
+			} 
+			return false;
+	
+			}
+		}
+		return true;
         
 	}
 
 	public static void main(String[] args) {
 
-		String s="a234";
+		String s="abc";
 		boolean answer=solution(s);
 		System.out.println(answer);
 	}

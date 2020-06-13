4e6ccd7f8bdc298a3c6c3db4be06f22160db1d70
qkrwlsaud <qkrwlsaud0118@gmail.com>
2020-06-14 00:19:06 +0900

    p와 y 개수 찾기

--- Level1_EX1/src/ex11_string4/Solution1.java
+++ Level1_EX1/src/ex11_string4/Solution1.java
@@ -5,19 +5,29 @@ public class Solution1 {
 	static boolean solution(String s) {
         boolean answer = true;
         char check=s.charAt(0);
+        int count=0;
        
-        int count;
+        
         for(int i=1;i<s.length();i++) {
-   
         	if(check>s.charAt(i)) { //check 문자가 소문자일 때
         		check-=32;
         		if(check==s.charAt(i)) { //같은 문자 일 때
         			System.out.println(s.charAt(i));
-        			count+=1;
-        		}	
+        		}count++;
+        	}else if(check<s.charAt(i)) {//check 문자가 대문자일 때
+        		System.out.println(check);
+        		check+=32;
+        		if(check==s.charAt(i)) { //같은 문자 일 때
+        			System.out.println(s.charAt(i));
+        		}count++;
+        	}else {
+        		count++;
         	}
-        	
         	check=s.charAt(i);
+        }
+       
+        if(count%2!=0 || count==0) {
+        	answer=false;
         }
         
         return answer;

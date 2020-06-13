daa2e94f1411a03da1c1a8bf7e1881a5c58f5591
qkrwlsaud <qkrwlsaud0118@gmail.com>
2020-06-13 23:17:31 +0900

    문자열 다루기 기본

---
+++ Level1_EX1/src/ex08_string1/Solution1.java
@@ -0,0 +1,30 @@
+package ex08_string1;
+
+public class Solution1 {
+	
+	
+	public static boolean solution(String s) {
+		
+		boolean answer=true;
+		if(s.length() ==4 || s.length()==6)
+			for(int i=0;i<s.length();i++) {
+				if(s.charAt(i) <'0' || s.charAt(i)>'9') {
+					answer=false;
+				}
+		}else {
+			answer=false;
+		}
+		
+	
+		return answer;
+        
+	}
+
+	public static void main(String[] args) {
+
+		String s="a234";
+		boolean answer=solution(s);
+		System.out.println(answer);
+	}
+
+}

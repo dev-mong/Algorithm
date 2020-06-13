ff6f1da48344de89015a573d65c81a515cf57450
qkrwlsaud <qkrwlsaud0118@gmail.com>
2020-06-13 23:17:51 +0900

    김서방 찾기

---
+++ Level1_EX1/src/ex09_string2/Solution1.java
@@ -0,0 +1,22 @@
+package ex09_string2;
+
+public class Solution1 {
+	
+    public  static String solution(String[] seoul) {
+        
+    	String answer = "";
+        
+        for(int i=0;i<seoul.length;i++) {
+        	if(seoul[i].equals("Kim")) {
+        		answer+="김서방은 "+i+"에 있다.";
+        	}
+        }
+        return answer;
+    }
+
+	public static void main(String[] args) {
+		String[] seoul= {"Jane", "Kim"};
+		System.out.println(solution(seoul));
+	}
+
+}

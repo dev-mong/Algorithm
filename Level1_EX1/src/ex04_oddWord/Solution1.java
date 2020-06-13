8a5c699e834501bd5eb2e066fd9ab293bb9ce5b3
qkrwlsaud <qkrwlsaud0118@gmail.com>
2020-06-13 23:16:11 +0900

    이상한 문자열 만들기

---
+++ Level1_EX1/src/ex04_oddWord/Solution1.java
@@ -0,0 +1,34 @@
+package ex04_oddWord;
+
+public class Solution1 {
+
+	public String solution(String s) {
+	     String answer = "";
+	          
+	          char[] charArr = s.toCharArray();
+	          int arrLength = charArr.length;
+	          char space = ' ';
+	          int wordNum = 1;
+	          for(int i =0; i<arrLength; i++) {
+	              if(charArr[i]==space) {
+	                  //공백일때처리
+	                  wordNum=1;
+	                  continue;
+	              }
+	              String temp = new String(charArr[i]+"");
+	              if(wordNum%2==0) {
+	                  //짝수 -> 대문자
+	                  temp = temp.toLowerCase();
+	                  charArr[i] = temp.charAt(0);
+	              }else {
+	                  //홀수 -> 소문자
+	                  temp = temp.toUpperCase();
+	                  charArr[i] = temp.charAt(0);
+	              }
+	              
+	              wordNum++;
+	          }
+	          answer = new String(charArr);
+	          return answer;
+	  }
+}

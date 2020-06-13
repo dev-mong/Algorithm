d5b7feebd1fb20331bae2b43d73489acc89f1610
qkrwlsaud <qkrwlsaud0118@gmail.com>
2020-06-13 23:17:18 +0900

    전화번호 가리기

---
+++ Level1_EX1/src/ex07_phoneNumber/Solution1.java
@@ -0,0 +1,31 @@
+package ex07_phoneNumber;
+
+public class Solution1 {
+
+	 public static String solution(String phone_number) {
+	        String answer = "";
+	 
+	    	for(int i=0;i<phone_number.length()-4;i++) {
+	    		
+	    		answer+="*";
+	    
+	    	}
+	    	
+	    	answer+=phone_number.substring(phone_number.length()-4,phone_number.length());
+	    	
+	        return answer;
+	 }
+	
+	public static void main(String[] args) {
+		
+		String phone_number="01033334444";
+
+		
+		phone_number=solution(phone_number);
+		
+		
+		System.out.println(phone_number);
+		
+	}
+
+}

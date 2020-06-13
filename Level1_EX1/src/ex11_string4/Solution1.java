5ae64143c90b4bbcf5c91934025ff8e41b3ce9b4
qkrwlsaud <qkrwlsaud0118@gmail.com>
2020-06-14 00:34:19 +0900

    문자열 내 p와 y의 개수

--- Level1_EX1/src/ex11_string4/Solution1.java
+++ Level1_EX1/src/ex11_string4/Solution1.java
@@ -4,29 +4,21 @@ public class Solution1 {
 
 	static boolean solution(String s) {
         boolean answer = true;
-        char check=s.charAt(0);
-        int count=0;
        
         
-        for(int i=1;i<s.length();i++) {
-        	if(check>s.charAt(i)) { //check 문자가 소문자일 때
-        		check-=32;
-        		if(check==s.charAt(i)) { //같은 문자 일 때
-        			System.out.println(s.charAt(i));
-        		}count++;
-        	}else if(check<s.charAt(i)) {//check 문자가 대문자일 때
-        		System.out.println(check);
-        		check+=32;
-        		if(check==s.charAt(i)) { //같은 문자 일 때
-        			System.out.println(s.charAt(i));
-        		}count++;
-        	}else {
-        		count++;
-        	}
-        	check=s.charAt(i);
-        }
+        int pcount=0;
+        int ycount=0;
        
-        if(count%2!=0 || count==0) {
+        for(int i=0;i<s.length();i++) {
+        	if(s.charAt(i)=='P' || s.charAt(i)=='p') {
+        		pcount++;
+        	}
+        	else if(s.charAt(i)=='Y' || s.charAt(i)=='y') {
+        		ycount++;
+        	}
+        }
+        
+        if(pcount!=ycount) {
         	answer=false;
         }
         

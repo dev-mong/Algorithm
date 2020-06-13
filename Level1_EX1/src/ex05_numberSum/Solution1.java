b7eec34a39f2921af3fb58a21750124826097bb8
qkrwlsaud <qkrwlsaud0118@gmail.com>
2020-06-13 23:16:22 +0900

    평균 구하기

---
+++ Level1_EX1/src/ex05_numberSum/Solution1.java
@@ -0,0 +1,22 @@
+package ex05_numberSum;
+
+public class Solution1 {
+
+	 public static int solution(int n) {
+	        int answer = 0;
+	        while(n != 0) {
+	        	answer+=n%10;
+	        	n=n/10;
+	        }
+	        return answer;
+	    }
+	
+	public static void main(String[] args) {
+
+		int a=987;
+		a=solution(a);
+		System.out.println(a);
+		
+	}
+
+}

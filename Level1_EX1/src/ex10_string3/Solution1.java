820a3455ff778d872d88f9ad6aae5f0179a6dd46
qkrwlsaud <qkrwlsaud0118@gmail.com>
2020-06-13 23:18:07 +0900

    가운데 글자 가져오기

---
+++ Level1_EX1/src/ex10_string3/Solution1.java
@@ -0,0 +1,27 @@
+package ex10_string3;
+
+public class Solution1 {
+
+   public static String solution(String s) {
+        String answer = "";
+        int index=s.length()/2;
+        int num=s.length()%2;
+        
+        if(num != 0) { //홀수
+        	answer+=s.charAt(index);
+        }else if(num==0) {//짝수
+        	answer+=s.charAt(index-1);
+        	answer+=s.charAt(index);
+        }
+        
+        return answer;
+    }
+	
+	public static void main(String[] args) {
+		
+		String s="qwer";
+		
+		System.out.println(solution(s));
+	}
+
+}

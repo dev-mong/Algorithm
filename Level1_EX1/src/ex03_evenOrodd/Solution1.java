4ffd45ac87a1b98512d1f0cc960dd1a59720030f
qkrwlsaud <qkrwlsaud0118@gmail.com>
2020-06-13 23:15:12 +0900

    짝수 홀수

---
+++ Level1_EX1/src/ex03_evenOrodd/Solution1.java
@@ -0,0 +1,24 @@
+package ex03_evenOrodd;
+
+public class Solution1 {
+
+   public static String solution(int num) {
+        String answer = "";
+        
+        if(num%2==0) {
+        	answer+="Even";
+        }else if(num%2!=0) {
+        	answer+="Odd";
+        }
+        
+        return answer;
+    }
+	public static void main(String[] args) {
+		int a=3;
+		int b=4;
+		
+		String answer =solution(a);
+		System.out.println(answer);
+	}
+
+}

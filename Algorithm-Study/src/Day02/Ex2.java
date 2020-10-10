4c6bf1c5b89e8fc85e824a0c0f2409d183d3b158
qkrwlsaud <qkrwlsaud0118@gmail.com>
2020-10-10 15:07:00 +0900

    위쪽과 왼쪽 덧셈, 곱셈표

---
+++ Algorithm-Study/src/Day02/Ex2.java
@@ -0,0 +1,29 @@
+package Day02;
+
+//예제 13
+//위쪽과 왼쪽 덧셈을 출력하는 덧셈표 
+
+public class Ex2 {
+
+	public static void main(String[] args) {
+		
+		System.out.print("  | ");
+		for(int k=1;k<=9;k++) {
+			System.out.print(k+"  ");
+		}
+		
+		System.out.println("\n--+----------------------------");
+		
+		int result = 0;
+		
+		for(int i=1;i<=9;i++) {
+			System.out.print(i+" |");
+			for(int j=1;j<=9;j++) {
+				result = i+j;
+				System.out.printf("%3d",result);
+			}
+			System.out.println();
+		}
+	}
+
+}

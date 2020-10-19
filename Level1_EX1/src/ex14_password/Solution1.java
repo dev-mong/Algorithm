371d91e79bbc3ce41961fb174aeee97e0e42ab09
qkrwlsaud <qkrwlsaud0118@gmail.com>
2020-10-19 21:06:16 +0900

    시저 암호

--- Level1_EX1/src/ex14_password/Solution1.java
+++ Level1_EX1/src/ex14_password/Solution1.java
@@ -4,9 +4,7 @@ public class Solution1 {
 
 	public static String solution(String s, int n) {
 		String answer = "";
-
 		char st = ' ';
-		int num;
 
 		for(int i=0;i<s.length();i++) {
 			
@@ -14,22 +12,19 @@ public class Solution1 {
 			 
 			 if(Character.isLowerCase(st)) { //소문자 일 때
 				 st = (char)(st+n);
-				 if(st >= 'z') {
+				 if(st > 'z') {
 					 st = (char)(st - 26);
 				 }
 				 
 			 } else if(Character.isUpperCase(st)) { //대문자 일 때
 				 st = (char)(st+n);
-				 if(st >= 'Z') {
+				 if(st > 'Z') {
 					 st = (char)(st - 26);
 				 }
 			 }
 			 
 			 answer += st;
 		}
-		
-		System.out.println(answer);
-		
 		
 		return answer;
 	}

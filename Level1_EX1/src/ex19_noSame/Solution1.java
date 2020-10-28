cd28508760cb4d838657d6c161a2ba8f1caf53f7
qkrwlsaud <qkrwlsaud0118@gmail.com>
2020-10-28 23:24:04 +0900

    같은 숫자는 싫어

---
+++ Level1_EX1/src/ex19_noSame/Solution1.java
@@ -0,0 +1,38 @@
+package ex19_noSame;
+
+//같은 숫자는 싫어 
+
+public class Solution1 {
+
+	public static int[] solution(int[] arr) {
+		int[] answer = new int[arr.length];
+		int temp = arr[0];
+		answer[0] = arr[0];
+		int cnt = 1;
+	
+		for (int i = 1; i < arr.length; i++) {
+			if (temp == arr[i]) {
+				continue;
+			}
+			answer[cnt] = arr[i];
+			temp = arr[i];
+			cnt++;
+		}
+
+		int[] result = new int[cnt];
+		for (int i = 0; i < cnt; i++) {
+			result[0] = answer[i];
+		}
+
+		return answer;
+	}
+
+	public static void main(String[] args) {
+
+		int[] arr = { 1, 1, 3, 3, 0, 1, 1 };
+
+		solution(arr);
+
+	}
+
+}
